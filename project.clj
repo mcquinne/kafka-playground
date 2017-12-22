31a90c5a22ed212a7b5cfd2ed928780164ee82f3
Evan McQuinn <evan.mcquinn@colorado.edu>
2017-12-21 17:12:44 -0700

    Simple pub/sub setup

--- project.clj
+++ project.clj
@@ -2,7 +2,12 @@
   :description "For learning about kafka"
   :license {:name "GPL v3"
             :url "http://www.gnu.org/licenses/gpl.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.apache.kafka/kafka_2.12 "1.0.0"
+                  :exclusions [javax.jms/jms
+                               com.sun.jdmk/jmxtools
+                               com.sun.jmx/jmxri]]]
   :main ^:skip-aot kafka-playground.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[proto-repl "0.3.1"]]}})
--- src/kafka_playground/core.clj
+++ src/kafka_playground/core.clj
@@ -1,7 +1,46 @@
 (ns kafka-playground.core
+  (:import (org.apache.kafka.clients.consumer KafkaConsumer)
+           (org.apache.kafka.clients.producer KafkaProducer ProducerRecord)
+           (java.util Properties)
+           (java.util.concurrent Executors))
   (:gen-class))
 
+(defn- create-producer
+  "Creates a kafka producer"
+  []
+  (let [props (Properties.)]
+    (doto props
+          (.put "bootstrap.servers" "localhost:9092")
+          (.put "key.serializer" "org.apache.kafka.common.serialization.StringSerializer")
+          (.put "value.serializer" "org.apache.kafka.common.serialization.StringSerializer")
+          (.put "client.id" "producer")
+          (.put "acks" "1"))
+    (KafkaProducer. props)))
+
+(defn- create-consumer
+  "Creates a kafka consumer, duh"
+  []
+  (let [props (Properties.)]
+    (doto props
+          (.put "bootstrap.servers" "localhost:9092")
+          (.put "key.deserializer" "org.apache.kafka.common.serialization.StringDeserializer")
+          (.put "value.deserializer" "org.apache.kafka.common.serialization.StringDeserializer")
+          (.put "group.id" "foo")
+          (.put "client.id" "consumer"))
+    (KafkaConsumer. props)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Do the things!"
   [& args]
-  (println "Hello, World!"))
+  (let [producer (create-producer)
+        consumer (create-consumer)
+        topic "topic"
+        msg (ProducerRecord. topic "abc" "this is a message")]
+    (println @(.send producer msg))
+    (.subscribe consumer [topic])
+    (let [it (.iterator (.poll consumer 10000))]
+      (while (.hasNext it)
+        (as-> (.next it) msg
+              (println (str "Received message " (.key msg) ": " (.value msg))))))
+    (.close producer)
+    (.close consumer)))
