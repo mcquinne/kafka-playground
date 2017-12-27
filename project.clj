a6e63af6f11836179e592cdb53744c4b98106645
Evan McQuinn <evan.mcquinn@colorado.edu>
2017-12-27 14:54:10 -0700

    Set up a server which takes json and publishes it to kafka

--- project.clj
+++ project.clj
@@ -6,8 +6,14 @@
                  [org.apache.kafka/kafka_2.12 "1.0.0"
                   :exclusions [javax.jms/jms
                                com.sun.jdmk/jmxtools
-                               com.sun.jmx/jmxri]]]
+                               com.sun.jmx/jmxri]]
+                 [ring/ring-core "1.6.3"]
+                 [ring/ring-json "0.4.0"]
+                 [ring/ring-jetty-adapter "1.6.3"]]
+  :plugins [[lein-ring "0.8.10"]]
+  :ring {:handler kafka-playground.core/wrapped-handler}
   :main ^:skip-aot kafka-playground.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[proto-repl "0.3.1"]]}})
+             :dev {:dependencies [[proto-repl "0.3.1"]
+                                  [org.clojure/tools.namespace "0.2.11"]]}})
--- src/kafka_playground/core.clj
+++ src/kafka_playground/core.clj
@@ -3,6 +3,8 @@
            (org.apache.kafka.clients.producer KafkaProducer ProducerRecord)
            (java.util Properties)
            (java.util.concurrent Executors))
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]])
   (:gen-class))
 
 (defn- create-producer
@@ -29,9 +31,9 @@
           (.put "client.id" "consumer"))
     (KafkaConsumer. props)))
 
-(defn -main
-  "Do the things!"
-  [& args]
+(defn pub-and-sub
+  "Sends a message, then subscribes to it and pulls it back down"
+  []
   (let [producer (create-producer)
         consumer (create-consumer)
         topic "topic"
@@ -44,3 +46,26 @@
               (println (str "Received message " (.key msg) ": " (.value msg))))))
     (.close producer)
     (.close consumer)))
+
+(defn kafka-pub-handler
+  "Ring handler which publishes request body to kafka"
+  [request]
+  (let [producer (create-producer)
+        topic "topic"
+        msg (ProducerRecord. topic "abc" (str (:body request)))]
+    {:status 200
+     :body {:sent (str @(.send producer msg))}}))
+
+(def wrapped-handler (-> kafka-pub-handler
+                         (wrap-json-body {:keywords? true})
+                         (wrap-json-response)))
+
+(defn listen-and-pub
+  "Start a ring server and publish requests to kafka"
+  []
+  (run-jetty wrapped-handler {:port 8080}))
+
+(defn -main
+  "Do the things!"
+  [& args]
+  (listen-and-pub))
