297193f98a1d47d153e6a118fe76c70b5bff1169
Evan McQuinn <evan.mcquinn@colorado.edu>
2017-12-27 19:16:40 -0700

    Add a trivial streams application to print everyting that comes through

--- src/kafka_playground/core.clj
+++ src/kafka_playground/core.clj
@@ -1,14 +1,17 @@
 (ns kafka-playground.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
-            [kafka-playground.api :refer [kafka-pub-handler]])
+            [kafka-playground.api :refer [kafka-pub-handler]]
+            [kafka-playground.kafka :refer [create-printer-stream]])
   (:gen-class))
 
-(def main-handler (-> kafka-pub-handler
-                      (wrap-json-body {:keywords? true})
-                      (wrap-json-response)))
+(def main-handler
+  (-> kafka-pub-handler
+      (wrap-json-body {:keywords? true})
+      (wrap-json-response)))
 
 (defn -main
   "Do the things!"
   [& args]
+  (.start (create-printer-stream))
   (run-jetty main-handler {:port 3000}))
--- src/kafka_playground/kafka.clj
+++ src/kafka_playground/kafka.clj
@@ -1,7 +1,12 @@
 (ns kafka-playground.kafka
-  (:import (org.apache.kafka.clients.consumer KafkaConsumer ConsumerConfig)
-           (org.apache.kafka.clients.producer KafkaProducer ProducerConfig ProducerRecord))
+  (:import [org.apache.kafka.clients.consumer KafkaConsumer ConsumerConfig]
+           [org.apache.kafka.clients.producer KafkaProducer ProducerConfig ProducerRecord]
+           [org.apache.kafka.streams.kstream Printed]
+           [org.apache.kafka.streams StreamsBuilder KafkaStreams StreamsConfig]
+           [org.apache.kafka.common.serialization Serdes])
   (:gen-class))
+
+(def ^String topic "topic")
 
 (defn create-producer
   "Creates a kafka producer"
@@ -22,11 +27,25 @@
     ConsumerConfig/GROUP_ID_CONFIG "foo"
     ConsumerConfig/CLIENT_ID_CONFIG "consumer"}))
 
-(def topic "topic")
-
 (defn create-record
   [key value]
   (ProducerRecord. topic key value))
+
+(defn create-stream-config
+  []
+  (StreamsConfig.
+   {StreamsConfig/APPLICATION_ID_CONFIG, "stream-app"
+    StreamsConfig/BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"
+    StreamsConfig/KEY_SERDE_CLASS_CONFIG,   (.getName (.getClass (Serdes/String)))
+    StreamsConfig/VALUE_SERDE_CLASS_CONFIG, (.getName (.getClass (Serdes/String)))}))
+
+(defn create-printer-stream
+  []
+  (let [config (create-stream-config)
+        builder (StreamsBuilder.)
+        stream (.stream builder topic)]
+    (.print stream (Printed/toSysOut))
+    (KafkaStreams. (.build builder) config)))
 
 (defn pub-and-sub
   "Sends a message, then subscribes to it and pulls it back down"
